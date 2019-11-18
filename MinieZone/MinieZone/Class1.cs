23355baa674f9d9bbc3e8d78f43aa6ecc0719458
Baboulinet67 <quentin.barabinot@orange.fr>
2019-11-18 09:31:05 +0100

    Creation

---
+++ MinieZone/MinieZone/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MinieZone
+{
+    public class Class1
+    {
+    }
+}
---
+++ MinieZone/MinieZoneConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MinieZoneConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
